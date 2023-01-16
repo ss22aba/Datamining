94756bcefa8583452ee89f01efd0bef73b131ff8
ss22aba <117084208+ss22aba@users.noreply.github.com>
2023-01-17 04:08:55 +0530

    Graph added

--- datamining.py
+++ datamining.py
@@ -9,89 +9,57 @@ Created on Tue Jan 10 01:23:08 2023
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import confusion_matrix, accuracy_score
-from matplotlib.colors import ListedColormap 
+import seaborn as sn
 
 # Importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
+
 print(dataset)
 X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
 # Feature Scaling
+from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # Training the Naive Bayes model on the Training set
-
+from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
+
 print(y_pred)
 
 # Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
 ac = accuracy_score(y_test,y_pred)
 cm = confusion_matrix(y_test, y_pred)
+
 print(ac)
 print(cm)
 print('\nTrue Positives(TP) = ', cm[0,0])
+
 print('\nTrue Negatives(TN) = ', cm[1,1])
+
 print('\nFalse Positives(FP) = ', cm[0,1])
+
 print('\nFalse Negatives(FN) = ', cm[1,0])
+
 plt.title("Confusion matrix")
-#plt.imshow(cm, cmap='binary')
+plt.imshow(cm, cmap='binary')
 
-# Visualize confusion matrix using Matplotlib
-fig, ax = plt.subplots(figsize=(4,4))
-ax.matshow(cm, cmap=plt.cm.Greens, alpha=1.0)
-for i in range(cm.shape[0]):
-    for j in range(cm.shape[1]):
-        ax.text(x=j, y=i,s=cm[i, j], va='center', ha='center', size='large')
-plt.xlabel('Predictions', fontsize=14)
-plt.ylabel('Actuals', fontsize=14)
-plt.title('Confusion Matrix', fontsize=18)
-plt.show()
+# visualize confusion matrix with seaborn heatmap
 
-# Visualising the Training set results 
-x_set, y_set = X_train, y_train 
-X1, X2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),  
-                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))  
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),  
-             alpha = 0.5, cmap = ListedColormap(('grey', 'pink')))  
-plt.xlim(X1.min(), X1.max())  
-plt.ylim(X2.min(), X2.max())  
-for i, j in enumerate(np.unique(y_set)):  
-    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],  
-                c = ListedColormap(('grey', 'pink'))(i), label = j)  
-plt.title('Naive Bayes (Training set)')  
-plt.xlabel('Age')  
-plt.ylabel('Estimated Salary')  
-plt.legend()  
-plt.show()   
+cm_matrix = pd.DataFrame(data=cm, columns=['Actual Positive:1', 'Actual Negative:0'], 
+                                 index=['Predict Positive:1', 'Predict Negative:0'])
 
-# Visualising the Test set results  
-x_set, y_set = X_test, y_test  
-X1, X2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),  
-                         np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))  
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),  
-                 alpha = 0.5, cmap = ListedColormap(('grey', 'pink')))  
-plt.xlim(X1.min(), X1.max())  
-plt.ylim(X2.min(), X2.max())  
-for i, j in enumerate(np.unique(y_set)):  
-        plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],  
-                    c = ListedColormap(('grey', 'pink'))(i), label = j)  
-plt.title('Naive Bayes (test set)')  
-plt.xlabel('Age')  
-plt.ylabel('Estimated Salary')  
-plt.legend()  
-plt.show()  
-# https://www.kaggle.com/datasets/rakeshrau/social-network-ads
+sn.heatmap(cm_matrix, annot=True, fmt='d', cmap='YlGnBu')
+
